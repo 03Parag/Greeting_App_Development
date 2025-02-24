6b0e45ae262f834b6b6a10cac95bae0add60f802
Parag Agarwal <0111al211117.parag@gmail.com>
2025-02-24 16:28:13 +0530

    [Parag Agrawal] Added problem ability for the greeting app to delete a greeting messages in the repository

---
+++ com/example/Greeting_App_Development/UC8_Delete_Message/GreetingAppDevelopmentApplication_Delete.java
@@ -0,0 +1,11 @@
+package com.example.Greeting_App_Development.UC8_Delete_Message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingAppDevelopmentApplication_Delete {
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingAppDevelopmentApplication_Delete.class, args);
+    }
+}
