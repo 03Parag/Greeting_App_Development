b1d2a743cb5407108d86e7380e52f4242d6e8c31
Parag Agarwal <0111al211117.parag@gmail.com>
2025-02-24 13:03:15 +0530

    {Parag Agrawal] Added problem using greeting controller return JSON for different HTTP methods.

---
+++ com/example/Greeting_App_Development/UC1_Return_JSON/GreetingAppDevelopmentApplication_JSON.java
@@ -0,0 +1,13 @@
+package com.example.Greeting_App_Development.UC1_Return_JSON;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingAppDevelopmentApplication_JSON {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingAppDevelopmentApplication_JSON.class, args);
+	}
+
+}
