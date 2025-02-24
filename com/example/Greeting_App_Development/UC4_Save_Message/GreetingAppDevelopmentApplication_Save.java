ea91d6926e740858edf9863086b49b846e3925b3
Parag Agarwal <0111al211117.parag@gmail.com>
2025-02-24 13:54:32 +0530

    [Parag Agrawal] Added problem ability for the greeting app to save the  greeting message in the repository

---
+++ com/example/Greeting_App_Development/UC4_Save_Message/GreetingAppDevelopmentApplication_Save.java
@@ -0,0 +1,11 @@
+package com.example.Greeting_App_Development.UC4_Save_Message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingAppDevelopmentApplication_Save {
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingAppDevelopmentApplication_Save.class, args);
+    }
+}
