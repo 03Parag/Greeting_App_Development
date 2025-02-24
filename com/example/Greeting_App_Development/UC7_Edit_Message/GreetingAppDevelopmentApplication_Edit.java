3d39b1d923e44d6fcb4769407e5c255d2c938969
Parag Agarwal <0111al211117.parag@gmail.com>
2025-02-24 16:21:06 +0530

    [Parag Agrawal] Added problem ability for the greeting app to edit a greeting messages in the repository

---
+++ com/example/Greeting_App_Development/UC7_Edit_Message/GreetingAppDevelopmentApplication_Edit.java
@@ -0,0 +1,11 @@
+package com.example.Greeting_App_Development.UC7_Edit_Message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingAppDevelopmentApplication_Edit {
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingAppDevelopmentApplication_Edit.class, args);
+    }
+}
