23a90e5ea33320e8f227c27e7d30e138dd4aeb84
Parag Agarwal <0111al211117.parag@gmail.com>
2025-02-24 14:00:28 +0530

    [Parag Agrawal] Added problem ability for the greeting app to save the  greeting message in the repository

---
+++ com/example/Greeting_App_Development/UC4_Save_Message/Model/GreetingModel_Save.java
@@ -0,0 +1,17 @@
+package com.example.Greeting_App_Development.UC4_Save_Message.Model;
+
+public class GreetingModel_Save {
+    private String message;
+
+    public GreetingModel_Save(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
