b4e2a9c3e103783a79ac1b1bf38a15853644400a
Parag Agarwal <0111al211117.parag@gmail.com>
2025-02-24 13:10:05 +0530

    [Parag Agrawal] Added problem to extend  greeting controller to use services layer to get simple greeting  message ”Hello World”

---
+++ com/example/Greeting_App_Development/UC2_Service_Layer/GreetingAppDevelopmentApplication_Service.java
@@ -0,0 +1,13 @@
+package com.example.Greeting_App_Development.UC2_Service_Layer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingAppDevelopmentApplication_Service {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingAppDevelopmentApplication_Service.class, args);
+    }
+
+}
