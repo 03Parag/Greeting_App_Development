beb343013d3d5196862124966dbcb03c161c7e7e
Parag Agarwal <0111al211117.parag@gmail.com>
2025-02-24 13:49:27 +0530

    [Parag Agrawal] Added problem ability for the greeting app to give greeting message with name

---
+++ com/example/Greeting_App_Development/UC3_Using_Name/Model/GreetingModel_Name.java
@@ -0,0 +1,17 @@
+package com.example.Greeting_App_Development.UC3_Using_Name.Model;
+
+public class GreetingModel_Name {
+    private String message;
+
+    public GreetingModel_Name(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
