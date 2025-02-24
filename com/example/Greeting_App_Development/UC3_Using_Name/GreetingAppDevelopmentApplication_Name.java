07438a4b9e49d7ccfaf36887e7e7411398f02f98
Parag Agarwal <0111al211117.parag@gmail.com>
2025-02-24 13:47:28 +0530

    [Parag Agrawal] Added problem ability for the greeting app to give greeting message with name

---
+++ com/example/Greeting_App_Development/UC3_Using_Name/GreetingAppDevelopmentApplication_Name.java
@@ -0,0 +1,12 @@
+package com.example.Greeting_App_Development.UC3_Using_Name;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingAppDevelopmentApplication_Name {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingAppDevelopmentApplication_Name.class, args);
+    }
+}
