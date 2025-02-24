8fe123be771555d653b582a321b65e39ab87e259
Parag Agarwal <0111al211117.parag@gmail.com>
2025-02-24 16:04:04 +0530

    [Parag Agrawal] Added problem ability for the greeting app to find a greeting message by Id in the repository

---
+++ com/example/Greeting_App_Development/UC5_Find_Message/GreetingAppDevelopmentApplication_Find.java
@@ -0,0 +1,11 @@
+package com.example.Greeting_App_Development.UC5_Find_Message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingAppDevelopmentApplication_Find {
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingAppDevelopmentApplication_Find.class, args);
+    }
+}
