19df47e721b1160ebec86db0790137a9c44547b8
Parag Agarwal <0111al211117.parag@gmail.com>
2025-02-24 16:14:29 +0530

    [Parag Agrawal] Added problem ability for the greeting app to list all the greeting messages in the repository

---
+++ com/example/Greeting_App_Development/UC6_List_Message/GreetingAppDevelopmentApplication_List.java
@@ -0,0 +1,11 @@
+package com.example.Greeting_App_Development.UC6_List_Message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingAppDevelopmentApplication_List {
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingAppDevelopmentApplication_List.class, args);
+    }
+}
